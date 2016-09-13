c0d7c7e143566df57f476d5a577ec0c851d6f96e
Selby <david@selbys.org.uk>
2016-09-13 17:12:14 +0100

    Add 47x47 citation dataset to package

--- R/citation-data.R
+++ R/citation-data.R
@@ -1,12 +1,25 @@
-#' Citation data
+#' Academic citations dataset
 #'
-#' Taken from the \code{BradleyTerry2} package.
+#' A square matrix of citation counts between 47 academic journals in the field of statistics in 2010.
 #'
-#' A 4-by-4 table of citation counts between statistical journals.
+#'  Citations point from the column journal (\code{citing}) to the row journal (\code{cited}).
+#'  The \emph{article counts} (that is, the total number of publications from that journal in 2010) are available via \code{articles}.
 #'
 #' @docType data
 #'
 #' @usage data(citations)
 #'
 #' @keywords datasets
+#'
+#' @references
+#' Varin, C., Cattelan, M., & Firth, D. (2016).
+#' Statistical modelling of citation exchange between statistics journals.
+#' \emph{Journal of the Royal Statistical Society: Series A (Statistics in Society)},
+#' \bold{179(1)}, 1--63.
+#'
+#' @seealso \code{\link[BradleyTerry2]{citations}}, a similar, smaller dataset in \code{BradleyTerry2}
 "citations"
+
+#' @rdname citations
+#' @usage data(articles)
+"articles"
