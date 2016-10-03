81ac96cd8146b893cfa26a91890d704ffa65a08b
Selby <david@selbys.org.uk>
2016-10-03 10:18:40 +0100

    Add functions to compute variance-covariance matrix for BT model

---
+++ R/BradleyTerry.R
@@ -0,0 +1,86 @@
+#' Estimate the parameters of a Bradley-Terry model
+#'
+#' Estimate Bradley-Terry ability scores from paired comparison data using the I-LSR algorithm.
+#' Avoids using \code{glm} and seems to be faster, but only approximates maximum likelihood.
+#'
+#' The function uses iterative Luce Spectral Ranking (I-LSR) to approximate the maximum likelihood ability weights of the Bradley-Terry model.
+#' \emph{To do:} Add unit tests.
+#'
+#' @param C a square matrix of paired comparisons
+#' @param maxits The maximum number of iterations in the Zermelo algorithm
+#' @param tolerance The criterion for convergence of the algorithm
+#' @param sort logical. If \code{TRUE}, sort the weights in descending order
+#' @param verbose logical. If \code{TRUE}, show the number of iterations as a message
+#'
+#' @return A vector of estimated ability scores
+#'
+#' @examples
+#' ILSR(citations)
+#'
+#' @references
+#' Maystre, Lucas and Grossglauser, Matthias (2015).
+#' Fast and accurate inference of Plackett--Luce models.
+#' In \emph{Advances in Neural Information Processing Systems},
+#' 172--180.
+#'
+#' @export
+ILSR <- function(C, maxits = 100, tolerance = 1e-6, verbose = FALSE){
+  n <- nrow(C)
+  pi <- matrix(NA, nrow = maxits + 1, ncol = n)
+  colnames(pi) <- colnames(C)
+  pi[1, ] <- rep(1/n, n)
+
+  # Iterative Luce Spectral Ranking
+  for(k in 1:maxits) {
+    Q <- C / (pi[k,][row(C)] + pi[k,][col(C)])
+    P <- matrix(Q / colSums(Q), nrow(Q))
+    pi[k+1,] <- abs(rARPACK::eigs(P, 1)$vectors)
+    pi[k+1,] <- pi[k+1,] / sum(pi[k+1,])
+    change <- sqrt(sum((pi[k+1,] - pi[k,])^2))
+    if (change < tolerance) {
+      if(verbose) message(paste('Converged in', k, 'iterations'))
+      pi <- pi[1:(k+1),] # trim results
+      break
+    }
+  }
+  pi_out <- c(tail(pi, 1))
+  names(pi_out) <- colnames(C)
+  pi_out
+}
+
+#' Calculate the log-likelihood of a Bradley-Terry model
+#' @param mu an estimate of ability scores
+#' @param X a matrix of paired comparisons (weights)
+#' @family Bradley-Terry model utility functions
+loglikelihood <- function(mu, X) {
+  diag(X) <- 0
+  sum(diag(mu) %*% X - X * log(exp(mu)[row(X)] + exp(mu)[col(X)]))
+}
+
+#' Calculate the observed Fisher information of Bradley-Terry model estimates
+#'
+#' @importFrom numDeriv hessian
+#' @family Bradley-Terry model utility functions
+hessianBT <- function(mu, X) {
+  numDeriv::hessian(function(t) -loglikelihood(t, X), mu)
+}
+
+#' Calculate the variance-covariance matrix for a Bradley-Terry model
+#'
+#' A utility function for \code{\link{BradleyTerry}}.
+#'
+#' @return A Fisher information matrix
+#' @family Bradley-Terry model utility functions
+#' @examples
+#' # Compare results with BradleyTerry2:
+#' if(!requireNamespace('BradleyTerry2')) install.packages('BradleyTerry2')
+#' bincounts <- BradleyTerry2::countsToBinomial(citations)
+#' BT2fit <- BradleyTerry2::BTm(cbind(win1, win2), player1, player2, data = bincounts)
+#' BT2_mu <- c('AmS' = 0, coef(BT2fit)); BT2_mu <- BT2_mu - mean(BT2_mu)
+#' BT2_covariances <- vcov(BT2fit)
+#' my_covariances <- vcovBT(BT2_mu, citations)
+#' plot(BT2_covariances, my_covariances); abline(0, 1)
+#' @export
+vcovBT <- function(mu, X) {
+  solve(hessianBT(mu, X)[-1,-1])
+}
--- R/PageRank.R
+++ R/PageRank.R
@@ -3,16 +3,39 @@
 #' Given a (weighted) adjacency matrix, compute the PageRank: the
 #'   the stationary distribution of a random walk around the graph.
 #'
+#' PageRank is an eigenvector centrality metric, equivalent to Pinski & Narin's \emph{influence weight} (1976)
+#' with the addition of a damping factor \code{alpha}, which simulates a random surfer traversing the graph and
+#' teleporting at any time with probability \code{1 - alpha}. The effect of the damping factor is to smooth out
+#' any disconnected components or transient portions of the network.
+#'
+#' In bibliometrics, PageRank has also been implemented as the Eigenfactor Metric and as the SCImago Journal Rank.
+#'
 #' @param C a square matrix
 #' @param alpha a damping factor
-#' @param row2col by default, c_{ij} refers to the directed edge that
+#' @param row2col logical. by default, \code{C[i,j]} refers to the directed edge that
 #'   points from column j to row i. Set \code{TRUE} to
-#'   transpose so that edges lead from rows to columns instead
+#'   transpose the matrix \code{C} so that edges lead from rows to columns instead
 #' @param sort reorder the indices in descending order of PageRank
 #'
 #' @return A PageRank vector, scaled to sum to one
 #'
 #' @importFrom rARPACK eigs
+#'
+#' @examples
+#' PageRank(citations)
+#'
+#' # Scroogefactor, an estimator for the Bradley-Terry model
+#' PageRank(citations)/colSums(citations)
+#'
+#' @references
+#' Pinski, G., & Narin, F. (1976).
+#' Citation influence for journal aggregates of scientific publications: Theory, with application to the literature of physics.
+#' \emph{Information Processing & Management},
+#' 12(5), 297--312.
+#'
+#' Page, L., Brin, S., Motwani, R., & Winograd, T. (1999).
+#' The PageRank citation ranking: bringing order to the web.
+#' Technical Report, Stanford InfoLab.
 #'
 #' @export
 PageRank <- function(C,
