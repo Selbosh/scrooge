b5664104e440f183271bd4a1f5f8cddb625b132c
Selby <david@selbys.org.uk>
2016-10-04 12:53:07 +0100

    First attempt at BT pseudocounts

--- vignettes/quasisymmetry.R
+++ vignettes/quasisymmetry.R
@@ -15,3 +15,22 @@ PR <- PR / sum(PR) # Renormalise to sum to 1
 all.equal(SF, PR)
 all.equal(SF, attr(X, 'a'))
 
+## ----damping-------------------------------------------------------------
+D <- diag(Matrix::colSums(X))
+P <- X %*% solve(D)
+alpha <- 0.85
+n <- nrow(X)
+G <- alpha * P + (1 - alpha) / n
+X_2 <- (G %*% D) / alpha - matrix((1 / alpha - 1) / n, n, n) %*% D
+all.equal(as.matrix(X), as.matrix(X_2),
+          check.attributes = FALSE)
+
+## ------------------------------------------------------------------------
+X_pseudo <- Matrix::t(alpha * Matrix::t(X) + (1 - alpha) * Matrix::colSums(X) / n)
+plot(Scroogefactor(X, alpha), ILSR(X))
+abline(0, 1)
+cor(Scroogefactor(X, alpha), ILSR(X))
+plot(Scroogefactor(X, alpha), ILSR(X_pseudo))
+abline(0, 1)
+cor(Scroogefactor(X, alpha), ILSR(X_pseudo))
+
