8e23615bb43451be56dd1ee8ed59cb98f20c0f0f
Selbosh <david@selbys.org.uk>
2017-06-23 16:23:20 +0100

    Remove 'methods' from NAMESPACE

--- R/profiles.R
+++ R/profiles.R
@@ -21,7 +21,6 @@ cprofile <- function(x, self) UseMethod('cprofile', x)
 
 #' @rdname cprofile
 #' @importFrom Matrix diag Diagonal colSums
-#' @importFrom methods as
 #' @export
 cprofile.default <- function(x, self = TRUE) {
   x <- as(x, 'dgCMatrix')
