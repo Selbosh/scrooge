b2bd11b19c8314c5eb5a29075bd86808642bb56e
Selbosh <david@selbys.org.uk>
2017-08-14 15:58:46 +0100

    Add unit test for full network glm

--- tests/testthat/test-residuals.R
+++ tests/testthat/test-residuals.R
@@ -3,11 +3,21 @@ context("Profile and community residuals")
 dist <- as.dist(1 - cor(citations + t(citations) - diag(diag(citations))))
 clusters <- cutree(hclust(dist), h = 0.6)
 
-test_that("Vector of predicted citations is equivalent to from a null glm", {
+test_that("Predicted citations from a given journal are equivalent to from a null glm", {
   Bka_predicted <- fitted_citations('Bka', citations, clusters)
   Bka_residuals <- profile_residuals(Bka_predicted, citations[, 'Bka'])
-  Bka_null_glm <- glm(citations[, 'Bka'] ~ 0 + offset(log(Bka_predicted)), family = 'poisson')
+  Bka_null_glm <- glm(citations[, 'Bka'] ~ 0 + offset(log(Bka_predicted)), family = poisson)
 
   expect_true(Bka_predicted %==% fitted(Bka_null_glm))
   expect_true(Bka_residuals %==% resid(Bka_null_glm, type = 'pearson'))
+})
+
+
+test_that("Predicted citations for full network are equivalent to from a null glm", {
+  all_predicted <- fitted_citations(NULL, citations, clusters)
+  all_residuals <- profile_residuals(all_predicted, citations)
+  all_null_glm <- glm(c(citations) ~ 0 + offset(log(c(all_predicted))), family = poisson)
+
+  expect_true(all_predicted %==% fitted(all_null_glm))
+  expect_true(all(abs(c(all_residuals) - resid(all_null_glm, type = 'pearson')) <= 1e-7))
 })
